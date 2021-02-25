420be2871858264bc396570424cbf9f65e832fa0
Victor da Cruz <victor.da.cruz95@gmail.com>
2021-02-24 21:15:02 -0300

    Criado componente Header

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules',
-            jquery: 'modules/admin-lte/bootstrap/js/bootstrap',
+            jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
             bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
     },
@@ -40,7 +40,7 @@ module.exports = {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         },{
-            test: /\.woff|.woff2Z.ttf|.eot|.svg|.png|.jpg*.*$/, 
+            test: /\.woff|.woff2Z|.ttf|.eot|.svg|.png|.jpg*.*$/, 
             loader: 'file'
         }]
         
