ac0dc1158284e37fb0d0519ae02ac49c460f7562
Victor da Cruz <victor.da.cruz95@gmail.com>
2021-02-25 20:38:08 -0300

    Add componentes content, contentHeader, valueBox, grid, row / Iniciado integracao com redux

--- frontend/src/common/template/footer.jsx
+++ frontend/src/common/template/footer.jsx
@@ -4,7 +4,7 @@ export default props => (
     <footer className='main-footer'>
         <strong>
             Copyright &copy; 2021
-            <a href="http://cod3r.com.br" target='_blank'> Meu Site</a>.
+            <a href="http://cod3r.com.br" target='_blank'>  Victor Eduardo da Cruz</a>.
         </strong>
     </footer>
 )
