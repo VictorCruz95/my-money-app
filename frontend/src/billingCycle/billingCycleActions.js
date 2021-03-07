171aeab8e9ac3692cfc095a5e3978a4de0d0d835
Victor da Cruz <victor.da.cruz95@gmail.com>
2021-03-07 19:51:20 -0300

    Fim implementação da aba Inclusão

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,8 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
+import { showTabs, selectTab } from '../common/tab/tabActions'
+
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getList(){
@@ -10,9 +14,20 @@ export function getList(){
 }
 
 export function create(values) {
-    axios.post(`${BASE_URL}/billingCycles`, values)
-
-    return {
-        type: 'TEMP'
+    return dispath => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
+        .then(resp => {
+            toastr.success('Sucesso', 'Operação Realizada com sucesso.')
+            dispath([ // Somente é possível passar array de actions por causa do redux-multi
+                resetForm('billingCycleForm'),
+                getList(),
+                selectTab('tabList'),
+                showTabs('tabList', 'tabCreate')
+            ])
+        })
+        .catch(e => {
+            e.response.data.errors.forEach(error => toastr.error('Error', error))
+        })
     }
+    
 }
