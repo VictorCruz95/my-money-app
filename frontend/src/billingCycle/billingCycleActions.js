3c5d86a501c096c30d907ae186e2547906e5ac05
Victor da Cruz <victor.da.cruz95@gmail.com>
2021-03-07 20:56:09 -0300

    Ajustes para aba de altearação

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
-import { reset as resetForm } from 'redux-form'
+import { reset as resetForm, initialize } from 'redux-form'
 import { showTabs, selectTab } from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:3003/api'
+const INITIAL_VALUES = {}
 
 export function getList(){
     const request = axios.get(`${BASE_URL}/billingCycles`)
@@ -18,16 +19,28 @@ export function create(values) {
         axios.post(`${BASE_URL}/billingCycles`, values)
         .then(resp => {
             toastr.success('Sucesso', 'Operação Realizada com sucesso.')
-            dispath([ // Somente é possível passar array de actions por causa do redux-multi
-                resetForm('billingCycleForm'),
-                getList(),
-                selectTab('tabList'),
-                showTabs('tabList', 'tabCreate')
-            ])
+            dispath(init())
         })
         .catch(e => {
             e.response.data.errors.forEach(error => toastr.error('Error', error))
         })
-    }
-    
+    }    
+}
+
+export function showUpdate(billingCycle){
+    return [
+        showTabs('tabUpdate'),
+        selectTab('tabUpdate'),
+        initialize('billingCycleForm', billingCycle) // Inicializa o formulario com os dados
+    ]
+}
+
+export function init(){
+    // Somente é possível passar array de actions por causa do redux-multi
+    return [
+        showTabs('tabList','tabCreate'),
+        selectTab('tabList'),
+        getList(),
+        initialize('billingCycleForm', INITIAL_VALUES)
+    ]
 }
